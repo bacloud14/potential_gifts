12510448f89420a8fc5805ac0d0aa2e7898278e5
RJ Hill <infamous.rj@gmail.com>
2020-10-03 16:15:50 -0400

    Create personal_assets.sql

---
+++ data/personal_assets.sql
@@ -0,0 +1,10 @@
+INSERT INTO `generic_asset` (`id`, `name`, `type`, `ephemeral`, `atomic`, `valuable_exchange`)
+VALUES
+  (93,'Artwork',                            'entertaining', false,true, true),
+  (94,'Automobile',                         'other',        false,true, true),
+  (95,'Checking account',                   'other',        false,false,true),
+  (96,'Collectibles Electronics Insurance', 'symbolic',     true, false,true),
+  (97,'Jewelry',                            'entertaining', false,false,true),
+  (98,'Investment accounts',                'productive',   false,false,true),
+  (99,'Retirement account',                 'productive',   false,false,true),
+  (100,'Savings account',                   'productive',   false,false,true)
