9a294e12cf2b058766f68821ffae2273532496a0
RJ Hill <infamous.rj@gmail.com>
2020-10-03 16:11:19 -0400

    Update personal_assets.sql

--- data/personal_assets.sql
+++ data/personal_assets.sql
@@ -3,7 +3,7 @@ VALUES
   (93,'Artwork',                            'entertaining', false,true, true),
   (94,'Automobile',                         'other',        false,true, true),
   (95,'Checking account',                   'other',        false,false,true),
-  (96,'Collectibles Electronics Insurance', 'symbolic',     false,false,true),
+  (96,'Collectibles Electronics Insurance', 'symbolic',     true, false,true),
   (97,'Jewelry',                            'entertaining', false,false,true),
   (98,'Investment accounts',                'productive',   false,false,true),
   (99,'Retirement account',                 'productive',   false,false,true),
